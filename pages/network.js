a182cc6ab16f10650208f2fd6af82792b4316030
Dan Silver <dannysilver3@gmail.com>
2015-09-12 19:43:09 -0500

    new template

---
+++ pages/network.js
@@ -0,0 +1,22 @@
+var app = angular.module('app', ['ui.sortable']);
+
+var Layer = function (size, type) {
+  this.size = size
+  this.type = type
+};
+
+app.controller('ctrl', function ($scope) {
+  $scope.phones = 45
+  $scope.nn_layers = [new Layer(5), new Layer(20)]
+
+  $scope.addLayer = function(type) {
+    bootbox.prompt("How many neurons in this layer?", function(result) {
+      if (result == "" || result === null) return;
+      $scope.nn_layers.push(new Layer(result, type))
+      $scope.$apply()
+    })
+  }
+  $scope.removeLayer = function(index) {
+    $scope.nn_layers.splice(index, 1)
+  }
+});
