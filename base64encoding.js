eac497781addc731e4de0a9ae633bb1f0f20d715
Daniel Hanson <dhansonmu@gmail.com>
2015-09-13 14:52:07 -0400

    Fixed getPixelData function.

--- base64encoding.js
+++ base64encoding.js
@@ -4,11 +4,15 @@ function getPixelData() {
     canvas.width = img.width;
     canvas.height = img.height;
     canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+    var pixels = [];
 
     for (i = 0; i < canvas.height; i++) { 
         for(j = 0; j < canvas.width; j++) {
-            var pixelData = canvas.getContext('2d').getImageData(j, i, 1, 1).data;
-            console.log(pixelData);
-        } 
+            var rawPixelData = canvas.getContext('2d').getImageData(j, i, 1, 1).data;
+            var grayscalePixel = rawPixelData[0] * 0.21 + rawPixelData[1] * 0.72 + rawPixelData[2] * 0.07;
+            pixels.push(Math.round(grayscalePixel).toString());
+        }
     }
+
+    console.log(pixels);
 }
